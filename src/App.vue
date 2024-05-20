b460f8bdce66076dbf2951c1ef93763e78fd5e03
rikofranjes <rikofranjes@gmail.com>
2024-05-20 11:20:43 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Koko from './components/Koko.vue'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+  <HelloWorld />
+  <Koko />
+</template>
---
+++ src/components/Koko.vue
@@ -0,0 +1,78 @@
+<template>
+    
+<div>
+    <div class="inf">
+      <div class="card" @click="tampilkanData('PUBG', '  adalah gim battle royale yang dirancang oleh Brendan Greene. Secara resmi, gim ini dirilis secara global pada bulan Maret 2018. Awalnya, PUBG tersedia untuk platform PC Windows melalui Steam, dan kemudian tersedia untuk Xbox One dan perangkat mobile seperti sekarang ini. Dalam gameplay-nya, ada 100 pemain yang bertempur untuk menjadi pemenang. ')">
+        <span></span>
+        <div class="content">
+          <img src="/pubg.jpeg" alt="gambar" class="gambar">
+        </div>
+      </div>
+
+      <div class="card" @click="tampilkanData('Mobile Legends', 'Mobile Legends: Bang Bang atau ML merupakan game MOBA yang dirilis oleh Moontoon. Game ini bisa dimainkan di ponsel Android maupun IOS. Dalam game MOBA ini akan ada 10 pemain yang akan dibagi menjadi 2 tim. ')">
+        <span></span>
+        <div class="content">
+          <img src="/ml.webp" alt="gambar" class="gambar">
+        </div>
+      </div>
+
+      <div class="card" @click="tampilkanData('Free Fire', 'Gameplay Free Fire adalah dimana semua pemain dikerahkan dengan pesawat dan wajib melakukan terjun bebas. Tiap pemain juga harus mencari senjata dan peralatan medis ketika dikirim untuk bertarung melawan pemain lain dan bertahan hidup. Di tengah permainan nanti akan ada pesawat yang lewat dan meluncurkan airdrop dalam bentuk kotak besar berisi rompi anti peluru, helm, senjata khusus seperti AWM, Groza , M79 dan M249 ataupun senapan mesin. Menemukan airdrop juga tidak sulit.')">
+        <span></span>
+        <div class="content">
+          <img src="/ff.jpeg" alt="gambar" class="gambar">
+        </div>
+      </div>
+
+      <div class="card" @click="tampilkanData('Clash of Clans', 'adalah permainan video strategi seluler freemium yang dikembangkan dan diterbitkan oleh pengembang permainan Finlandia, Supercell. Permainan ini dirilis untuk platform iOS pada 2 Agustus 2012, dan di Google Play untuk Android pada 7 Oktober 2013. Permainan ini berlatarkan tema fantasi dunia persistent dimana pemainnya adalah kepala desa. Clash of Clans menugaskan pemain untuk membangun desa mereka sendiri menggunakan sumber daya yang diperoleh dari menyerang desa pemain lain atau memproduksinya di desa sendiri')">
+        <span></span>
+        <div class="content">
+          <img src="/coc.jpeg" alt="gambar" class="gambar">
+        </div>
+      </div>
+    </div>
+
+    <div v-if="dataTerpilih" class="tampilan-data">
+      <h2>{{ dataTerpilih.judul }}</h2>
+      <p>{{ dataTerpilih.informasi }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      dataTerpilih: null
+    };
+  },
+  methods: {
+    tampilkanData(judul, informasi) {
+      if (this.dataTerpilih && this.dataTerpilih.judul === judul) {
+        this.dataTerpilih = null;
+      } else {
+        this.dataTerpilih = { judul, informasi };
+      }
+    }
+  }
+};
+</script>
+
+
+
+<style>
+
+.tampilan-data {
+  position: fixed; /* Menggunakan posisi tetap */
+  bottom: 25px; /* Atur posisi sesuai kebutuhan */
+  right: 274px; /* Atur posisi sesuai kebutuhan */
+  height: 170px;
+  width: 1000px; /* Lebar elemen data */
+  padding: 1px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: #f9f9f9;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Tambahkan bayangan untuk efek */
+}
+</style>
+
